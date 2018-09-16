b5d77ef7a6662c05bfba02c09270bc050f5aba5c
僚介 <ryousuke@Kawashima-mba.local>
2018-09-16 15:21:40 +0900

    temp.sh update

--- temp/temp.sh
+++ temp/temp.sh
@@ -4,22 +4,21 @@ echo "This shellscript makes a working directry for analysis."
 echo "Current directry is"
 pwd
 curpath=`pwd`
+SCRIPT_DIR=`epp_temp`
 echo "**************************************************************"
-cd `dirname $0`
 read -p "Please input path and directory name: " name
-if [-n "$name"]; then
-echo "Directory name is empty"
-echo "Making tutorial directry"
-mkdir $curpath/tutorial
-mkdir $curpath/tutorial/out
-mkdir $curpath/tutorial/data
-cp -r $0/program $curpath/tutorial/
-else
+#if [-n "$name"]; then
+#echo "Directory name is empty"
+#echo "Making tutorial directry"
+#mkdir $curpath/tutorial
+#mkdir $curpath/tutorial/out
+#mkdir $curpath/tutorial/data
+#cp -r $SCRIPT_DIR/program $curpath/tutorial/
+#else
 echo "Now making"
 mkdir $curpath/$name
 mkdir $curpath/$name/out
 mkdir $curpath/$name/data
-cp -r $0/program $curpath/$name/
-fi
-cd $curpath
+cp -r $SCRIPT_DIR/program $curpath/$name/
+#fi
 echo "Program finished"
